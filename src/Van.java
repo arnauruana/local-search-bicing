a563f4f583aa0487b83fc84d977488568f6d6ba5
Arnau Ruana <arnauruana@outlook.com>
2019-10-14 00:01:22 +0200

    Van fixes   - updated: src/Van.java -> bug fixes

--- src/Van.java
+++ src/Van.java
@@ -1,6 +1,7 @@
 import java.awt.Point;
 import java.lang.Integer;
 import java.lang.String;
+import static java.lang.System.err;
 import static java.lang.System.out;
 
 
@@ -17,8 +18,64 @@ public class Van {
 
   // =============================== METHODS ================================ //
 
+  // ----------------------------- Constructors ----------------------------- //
+
+  public Van() {}
+
+  public Van(final Point position) {
+    this.position = new Point(position);
+  }
+
+  public Van(final Point position, final Integer numBikes) {
+    this(position);
+    this.setBikes(numBikes);
+  }
+
+  public Van(final Integer x, final Integer y) {
+    this(new Point(x, y));
+  }
+
+  public Van(final Integer x, final Integer y, final Integer numBikes) {
+    this(new Point(x, y), numBikes);
+  }
+
+  // ------------------------------ Modifiers ------------------------------- //
+
+  public void setPosition(final Point position) {
+    this.position.setLocation(position);
+  }
+
+  public void setPosition(final Integer x, final Integer y) {
+    this.position.setLocation(x, y);
+  }
+
+  public void setBikes(final Integer numBikes) {
+    this.checkCapacity(numBikes, "setBikes");
+    this.numBikes = numBikes;
+  }
+
+  // ----------------------------- Consultants ------------------------------ //
+
+  public Point getPosition() {
+    return this.position;
+  }
+
+  public Integer getCoordX() {
+    return (int)this.position.getX();
+  }
+
+  public Integer getCoordY() {
+    return (int)this.position.getY();
+  }
+
+  public Integer getNumBikes() {
+    return this.numBikes;
+  }
+
+  // ------------------------------ Auxiliary ------------------------------- //
+
   private void printError(final String errorMsg) {
-    out.println(errorMsg);
+    err.println(errorMsg);
   }
 
   private void checkCapacity(final Integer numBikes, final String function) {
@@ -42,63 +99,6 @@ public class Van {
     }
   }
 
-  // ----------------------------- Constructors ----------------------------- //
-
-  public Van() {}
-
-  public Van(final Point position) {
-    this.setPosition(position);
-  }
-
-  public Van(final Point position, final Integer numBikes) {
-    this(position);
-    this.setBikes(numBikes);
-  }
-
-  public Van(final Integer x, final Integer y) {
-    this.setPosition(x, y);
-  }
-
-  public Van(final Integer x, final Integer y, final Integer numBikes) {
-    this(x, y);
-    this.setBikes(numBikes);
-  }
-
-  // ------------------------------ Modifiers ------------------------------- //
-
-  public void setPosition(final Point position) {
-    this.position.setLocation(position);
-  }
-
-  public void setPosition(final Integer x, final Integer y) {
-    this.position.setLocation(x, y);
-  }
-
-  public void setBikes(final Integer numBikes) {
-    this.checkCapacity(numBikes, "setBikes");
-    this.numBikes = numBikes.intValue();
-  }
-
-  // ----------------------------- Consultants ------------------------------ //
-
-  public Point getPosition() {
-    return this.position;
-  }
-
-  public Integer getCoordX() {
-    return (int)this.position.getX();
-  }
-
-  public Integer getCoordY() {
-    return (int)this.position.getY();
-  }
-
-  public Integer getNumBikes() {
-    return this.numBikes;
-  }
-
-  // ------------------------------ Auxiliary ------------------------------- //
-
   public void moveTo(final Point position) {
     this.moveTo((int)position.getX(), (int)position.getY());
   }
@@ -108,15 +108,34 @@ public class Van {
   }
 
   public void takeBikes(final Integer numBikes) {
-    Integer sum = this.numBikes.intValue() + numBikes.intValue();
+    Integer sum = this.numBikes + numBikes;
     checkCapacity(sum, "takeBikes");
-    this.numBikes = sum.intValue();
+    this.numBikes = sum;
   }
 
   public void dropBikes(final Integer numBikes) {
-    Integer sub = this.numBikes.intValue() - numBikes.intValue();
+    Integer sub = this.numBikes - numBikes;
     checkCapacity(sub, "dropBikes");
     this.numBikes = sub.intValue();
   }
 
-}
+  // ---------------------------- Input / Output ---------------------------- //
+
+  public void print() {
+    out.println("[Van(print)]");
+      out.print("  ⤷ position: "); this.printPosition();
+      out.print("  ⤷ numBikes: "); this.printNumBikes();
+      out.print("  ⤷ CAPACITY: "); this.printCapacity();
+  }
+
+  public void printPosition() {
+    out.println(this.position);
+  }
+
+  public void printNumBikes() {
+    out.println(this.numBikes);
+  }
+
+  public void printCapacity() {
+    out.println(this.CAPACITY);
+  }
