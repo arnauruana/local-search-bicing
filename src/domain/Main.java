fd8bc406fe89cc61890b02b89fe7b03eafb60545
guillempla <guillem.pla@est.fib.upc.edu>
2019-10-24 10:57:54 +0200

    Main improvement.

--- src/domain/Main.java
+++ src/domain/Main.java
@@ -1,49 +1,49 @@
 package domain;
 
-import IA.Bicing.Estaciones;
 import aima.search.framework.Problem;
 import aima.search.framework.Search;
 import aima.search.framework.SearchAgent;
+import aima.search.framework.SuccessorFunction;
 import aima.search.informed.HillClimbingSearch;
 import aima.search.informed.SimulatedAnnealingSearch;
 
-import java.util.ArrayList;
-
 public class Main {
-
-    private static ArrayList initalizeRandomFleet(int nVans) {
-        ArrayList<Van> fleet = new ArrayList<>(nVans);
-        for (Van v: fleet) {
-
-        }
-        return fleet;
-    }
-
-    private static ArrayList initalizeFixedFleet(int nVans) {
-        ArrayList<Van> fleet = new ArrayList<>(nVans);
-        for (Van v: fleet) {
-
-        }
-        return fleet;
-    }
-
 
     private static State initializeState(String[] args) {
         int nest = Integer.parseInt(args[2]);
         int nbic = Integer.parseInt(args[3]);
         int dem  = Integer.parseInt(args[4]);
         int seed = Integer.parseInt(args[5]);
-        Estaciones estaciones = new Estaciones(nest, nbic, dem, seed);
-        ArrayList fleet = (args[1].equals("r"))
-                ? initalizeRandomFleet(Integer.parseInt(args[6]))
-                : initalizeFixedFleet(Integer.parseInt(args[6]));
-        return new State (estaciones, fleet);
+        int nVans = Integer.parseInt(args[6]);
+        return new State(nest, nbic, dem, seed, nVans);
     }
     public static void main(String[] args) throws Exception {
+        // Initialize initialState, Random or Fixed
         State initialState = initializeState(args);
-        Problem problem = new Problem(initialState, new Successor(), new Goal());
+        (args[1].equals("r"))
+            ? initialState.initalizeRandom()
+            : initialState.initalizeFixed();
 
-        Search search = (args[0].equals("h")) ? new HillClimbingSearch() : new SimulatedAnnealingSearch();
+        // Only for Simulated Annealing Search
+        int maxIt  = Integer.parseInt(args[7]);
+        int numIt  = Integer.parseInt(args[8]);
+        int k      = Integer.parseInt(args[9]);
+        int lambda = Integer.parseInt(args[10]);
+
+        // Initialize Search. (AIMA) and initialize Successor
+        Search search;
+        SuccessorFunction successor;
+        if (args[0].equals("h")) {
+            search = new HillClimbingSearch();
+            successor = new Successor();
+        }
+        else {
+            search = new SimulatedAnnealingSearch(maxIt, numIt, k, lambda);
+            successor = new SuccessorRandom();
+        }
+
+        // Initialize Problem. (AIMA)
+        Problem problem = new Problem(initialState, successor, new Goal());
 
         SearchAgent agent = new SearchAgent(problem, search);
 
