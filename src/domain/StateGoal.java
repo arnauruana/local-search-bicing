c989df4acdcf848b2f81b6a46a9e734b1d49b7f8
xKvothe <aleix.marro@est.fib.upc.edu>
2019-10-27 20:28:16 +0100

    GoalState fiex

--- src/domain/StateGoal.java
+++ src/domain/StateGoal.java
@@ -1,12 +1,26 @@
 package domain;
 
 import aima.search.framework.GoalTest;
+import aima.search.framework.HeuristicFunction;
+import aima.search.framework.Successor;
+import aima.search.framework.SuccessorFunction;
 
+import IA.Bicing.Estacion;
+import IA.Bicing.Estaciones;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class StateGoal implements GoalTest {
 
-    public boolean isGoalState(Object n) {
-        return false;
+    public boolean isGoalState(Object state) {
+        State board = (State) state;
+        ArrayList<Van> fleet = board.getFleet();
+        for (Van actV : fleet) {
+            if (actV.getIsVisited() == false) return false;
+
+        }
+        return true;
     }
 
 }
