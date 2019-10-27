32f3065fbaed80b1c0b00eaa06f7ae0615a964fa
guillempla <guillem.pla@est.fib.upc.edu>
2019-10-27 21:23:49 +0100

    Things

--- src/domain/Van.java
+++ src/domain/Van.java
@@ -1,19 +1,11 @@
 package domain;
 
-import java.lang.Integer;
-import java.lang.String;
-
-import static java.lang.System.err;
-import static java.lang.System.out;
-
 
 public class Van {
 
   // ============================== ATTRIBUTES ============================== //
 
   private Integer stationID;          // origin station identifier
-  private Integer numBikes = 0;       // number of bikes being transported
-  private boolean isVisited = false;  // van visited
 
   // ------------------------------------------------------------------------ //
 
@@ -33,67 +25,10 @@ public class Van {
     this.stationID = stationID;
   }
 
-  void setIsVisited(boolean b) {
-    this.isVisited = b;
-  }
-
   // ----------------------------- Consultants ------------------------------ //
 
   Integer getOriginStationID() {
     return this.stationID;
-  }
-
-  Integer getNumBikes() {
-    return this.numBikes;
-  }
-
-  Boolean getIsVisited() {
-    return this.isVisited;
-  }
-
-  // ------------------------------ Auxiliary ------------------------------- //
-
-  private static void printError(final String errorMsg) {
-    err.println(errorMsg);
-  }
-
-  private void checkCapacity(final Integer numBikes) {
-    String error = "[Van(setNumBikes)] ERROR: number of bikes ("
-            + numBikes.toString();
-    if (numBikes < 0) {
-      error = error + ") cannot be negative";
-      Van.printError(error);
-      System.exit(1);
-    }
-    else if (numBikes > 30) {
-      error = error + ") cannot be greater than capacity ("
-              + Van.CAPACITY.toString() + ")";
-      Van.printError(error);
-      System.exit(1);
-    }
-  }
-
-  // ---------------------------- Input / Output ---------------------------- //
-
-  private void printStationID() {
-    out.println(this.stationID);
-  }
-
-  private void printNumBikes() {
-    out.println(this.numBikes);
-  }
-
-  private void printCapacity() {
-    out.println(Van.CAPACITY);
-  }
-
-  // ------------------------------------------------------------------------ //
-
-  void print() {
-    out.println("[Van] INFO: printing attributes...");
-    out.print("  ⤷ stationID:\t"); this.printStationID();
-    out.print("  ⤷ numBikes:\t");  this.printNumBikes();
-    out.print("  ⤷ CAPACITY:\t");  this.printCapacity();
   }
 
 }
